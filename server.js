7a4865062713aca68a15bc588c205ab59c81f9f2
DanilDev <40500597+Danil-Dev@users.noreply.github.com>
2021-08-04 00:37:13 +0300

    Fix *Configure for server

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 // express
 const express = require('express');
 const app = express();
-app.use(cors())
+// app.use(cors())
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
