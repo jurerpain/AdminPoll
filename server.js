4c945ae50ed0ed8ad7ee1d845b83881e94f5d63f
DanilDev <40500597+Danil-Dev@users.noreply.github.com>
2021-08-04 01:06:01 +0300

    Add cors options

--- server.js
+++ server.js
@@ -4,11 +4,22 @@ const findUserIndex = require("./backend/utils/findUserIndex");
 const findUser = require("./backend/utils/findUser");
 const path = require('path');
 const cors = require('cors');
+const whitelist = ['http://black.poladmin.pp.ua', 'http://poladmin.pp.ua']
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+    methods: ['GET', 'POST']
+}
 
 // express
 const express = require('express');
 const app = express();
-// app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
